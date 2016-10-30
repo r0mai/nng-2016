8a2987c8a4f47d8be328a5443fc3c506bda426c0
Matthias Vegh <matyas.vegh@gmail.com>
2016-10-30 20:05:23 +0100

    Add more verification for cat

--- qualif/cat/cat.cpp
+++ qualif/cat/cat.cpp
@@ -1,15 +1,11 @@
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <random>
 #include <vector>
 
-#include <algorithm>
-
-#include <boost/range/join.hpp>
 
 #include "solution.hpp"
-
-
-// Global, as NNG hasn't heard of std::function.
-std::vector<bool> balls = {true, true, false, false};
 
 std::string printSolution(const std::vector<size_t>& solution) {
 	return '{' +std::accumulate(solution.begin(), solution.end(),
@@ -23,25 +19,65 @@ std::string printSolution(const std::vector<size_t>& solution) {
 			}) + '}';
 }
 
+class Tester {
+	static std::vector<bool> currentBalls;
+	static bool checker(const std::vector<size_t>& ballIndices) {
+		for (size_t index: ballIndices) {
+			if (currentBalls[index]) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-bool checker(const std::vector<size_t>& ballIndices) {
-	for (size_t index: ballIndices) {
-		if (balls[index]) {
-			return true;
+	std::vector<size_t> getIndices() const {
+		std::vector<size_t> result;
+		for(std::size_t index = 0; index < currentBalls.size(); ++index) {
+			if(currentBalls[index]) {
+				result.push_back(index);
+			}
+		}
+		return result;
+	}
+
+public:
+	void setBalls(std::vector<bool> balls) {
+		currentBalls = std::move(balls);
+	}
+
+	void verify() {
+		auto expectedResult = getIndices();
+		auto result = FindRadioactiveBalls(currentBalls.size(),
+				expectedResult.size(),
+				&checker);
+
+		if (expectedResult != result) {
+			std::cerr << "Implementation not correct" << std::endl;
+			std::cerr << printSolution(result) << std::endl;
+			std::exit(1);
 		}
 	}
-	return false;
+
+};
+
+std::vector<bool> Tester::currentBalls;
+
+std::vector<bool> generateRandomSample(std::size_t length) {
+	static std::random_device rd;
+	static std::mt19937 gen{rd()};
+	std::bernoulli_distribution coinToss{0.5};
+	std::vector<bool> result;
+	std::generate_n(std::back_inserter(result), length, std::bind(coinToss, gen));
+	return result;
 }
 
 int main() {
 
-	std::vector<size_t> expectedResult = {0, 1};
-	auto result = FindRadioactiveBalls(balls.size(), expectedResult.size(),
-			&checker);
+	Tester tester;
 
-	if (expectedResult != result) {
-		std::cerr << "Implementation not correct" << std::endl;
-		std::cerr << printSolution(result) << std::endl;
-		std::exit(1);
+	for(std::size_t length = 1; length < 100; ++length) {
+		tester.setBalls(generateRandomSample(length));
+		tester.verify();
 	}
+	std::cerr << "Verification passed" << std::endl;
 }
