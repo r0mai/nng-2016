06471449748722e69e24e03a0ec82d415bdb79cb
Matthias Vegh <matyas.vegh@gmail.com>
2016-10-30 19:22:54 +0100

    Add naive implementation for cat

--- qualif/cat/cat.cpp
+++ qualif/cat/cat.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <vector>
+
+#include <algorithm>
+
+#include <boost/range/join.hpp>
 
 #include "solution.hpp"
 
+
+// Global, as NNG hasn't heard of std::function.
+std::vector<bool> balls = {true, true, false, false};
+
+std::string printSolution(const std::vector<size_t>& solution) {
+	return '{' +std::accumulate(solution.begin(), solution.end(),
+			std::string{},
+			[](std::string output, size_t element) {
+				if (!output.empty()) {
+					output += ", ";
+				}
+				output += std::to_string(element);
+				return output;
+			}) + '}';
+}
+
+
+bool checker(const std::vector<size_t>& ballIndices) {
+	for (size_t index: ballIndices) {
+		if (balls[index]) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
+
+	std::vector<size_t> expectedResult = {0, 1};
+	auto result = FindRadioactiveBalls(balls.size(), expectedResult.size(),
+			&checker);
+
+	if (expectedResult != result) {
+		std::cerr << "Implementation not correct" << std::endl;
+		std::cerr << printSolution(result) << std::endl;
+		std::exit(1);
+	}
 }
--- qualif/cat/solution.cpp
+++ qualif/cat/solution.cpp
@@ -1,10 +1,20 @@
 #include "solution.hpp"
 
+#include <iostream>
+
 std::vector<size_t> FindRadioactiveBalls(size_t NumberOfBalls,
 		size_t RadioActiveBalls,
 		bool (*TestFunction)(const std::vector<size_t>& BallsToTest)) {
-	(void)NumberOfBalls;
+
 	(void)RadioActiveBalls;
-	(void) TestFunction;
-	return {};
+
+	std::vector<size_t> result;
+
+	for (size_t ball = 0; ball < NumberOfBalls; ++ball) {
+		if (TestFunction({ball})) {
+			result.push_back(ball);
+		}
+	}
+
+	return result;
 }
