6f4ea01cc3a6b4a148e119f78d7fb6891dedc96e
Andras Kucsma <andras.kucsma@prezi.com>
2016-10-31 13:08:41 +0100

    Add initial stuff for buildings

---
+++ qualif/buildings/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+void CalculateBuildOrder(
+    const std::vector<std::vector<int>>& buildings,
+    std::vector<std::pair<size_t, size_t>>& solutions)
+{
+
+}
+
+#ifdef LOCAL
+int main() {
+
+}
+#endif
