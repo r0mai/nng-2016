01a414aaa008e08368ab786b4f780a5e5e2ecf1c
Andras Kucsma <andras.kucsma@prezi.com>
2016-11-01 21:20:59 +0100

    C++ify Gida's python stuff

--- qualif/buildings/main.cpp
+++ qualif/buildings/main.cpp
@@ -7,11 +7,20 @@
 #include <chrono>
 #include <unordered_set>
 
+#include <boost/optional.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/container/static_vector.hpp>
 #include <boost/functional/hash.hpp>
 
 using Buildings = std::vector<std::vector<int>>;
 using Command = std::pair<size_t, size_t>;
+
+struct Point {
+    Point() = default;
+    Point(int y, int x) : y(y), x(x) {}
+    int y = 0;
+    int x = 0;
+};
 
 std::ostream& operator<<(std::ostream& os, const Buildings& buildings) {
     auto h = buildings.size();
@@ -26,6 +35,85 @@ std::ostream& operator<<(std::ostream& os, const Buildings& buildings) {
     }
     return os;
 }
+
+class Parcel {
+public:
+    Parcel(const Buildings& buildings) :
+        buildings(buildings),
+        h(buildings.size()),
+        w(buildings.front().size())
+    {
+        rewrite();
+    }
+
+    const Buildings& getBuildings() const { return buildings; }
+    const std::vector<Command>& getHistory() const { return history; }
+
+    void rewrite() {
+        for (int y = 1; y < h-1; ++y) {
+            for (int x = 1; x < w-1; ++x) {
+                if (buildings[y][x] == 1) { buildings[y][x] = 5; }
+            }
+        }
+    }
+
+    boost::container::static_vector<Point, 4> getNeighbors(const Point& p) {
+        boost::container::static_vector<Point, 4> result;
+
+        if (p.x > 0)   { result.push_back({p.y, p.x-1}); }
+        if (p.y > 0)   { result.push_back({p.y-1, p.x}); }
+        if (p.x < w-1) { result.push_back({p.y, p.x+1}); }
+        if (p.y < h-1) { result.push_back({p.y+1, p.x}); }
+
+        return result;
+    }
+
+    void unbuild(const Point& p) {
+        assert(buildings[p.y][p.x] == 1);
+        buildings[p.y][p.x] = 0;
+        history.push_back({p.y, p.x});
+
+        for (const Point& n : getNeighbors(p)) {
+            int& height = buildings[n.y][n.x];
+            assert(height == 0 || height > 1);
+            height = std::max(height - 1, 0);
+        }
+    }
+
+    boost::optional<Point> FindHeight(int height) {
+        for (int y = 0; y < h; ++y) {
+            for (int x = 0; x < w; ++x) {
+                if (buildings[y][x] == height) {
+                    return Point{y, x};
+                }
+            }
+        }
+        return boost::none;
+    }
+
+    bool demolish() {
+        Point p;
+        auto p1 = FindHeight(1);
+        if (!p1) {
+            auto p5 = FindHeight(5);
+            if (!p5) {
+                return false;
+            }
+            p = *p5;
+            buildings[p.y][p.x] = 1;
+        } else {
+            p = *p1;
+        }
+        unbuild(p);
+        return true;
+    }
+
+private:
+    Buildings buildings;
+    std::vector<Command> history;
+    int w;
+    int h;
+};
 
 void buildOuterLayer(int& building) {
     if (building != 0 && ++building == 5) {
@@ -107,8 +195,6 @@ std::unordered_set<Buildings, BuildingsHash> buildingsHistory;
 
 bool backtrackRecurse(int h, int w, Buildings& buildings, std::vector<Command>& commands, int depth) {
     if (!buildingsHistory.insert(buildings).second) {
-#ifdef LOCAL
-#endif
         return false;
     }
 
@@ -167,7 +253,15 @@ void CalculateBuildOrder(
     std::vector<Command>& commands)
 {
     commands.clear();
+#if 1
+    Parcel parcel(buildings);
+    while (parcel.demolish()) {
+        std::cout << parcel.getBuildings() << std::endl;
+    }
 
+    commands = parcel.getHistory();
+    std::reverse(begin(commands), end(commands));
+#else
     auto h = buildings.size();
     auto w = buildings.front().size();
 
@@ -176,6 +270,7 @@ void CalculateBuildOrder(
     auto copy = buildings;
     backtrackRecurse(h, w, copy, commands, 1);
     std::reverse(begin(commands), end(commands));
+#endif
 }
 
 Buildings applyBuildOrder(int h, int w, const std::vector<Command>& commands) {
