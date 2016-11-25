fdaf79c8903703d7e240016959aef9672ca20781
Matyas Vegh <matyas.vegh@ericsson.com>
2016-11-25 11:29:33 +0100

    Add Runner for Agent

---
+++ final/runner.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PID=
+
+cd "${REPOSITORY}"
+
+isRebuildNeeded() {
+  git remote update &>/dev/null
+  diffcount=$(git rev-list HEAD...origin/master --count)
+  if [ "$diffcount" -gt "0" ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+rebuild() {
+  git checkout origin/master &>/dev/null
+  make -j8 -C build &>/dev/null
+}
+
+restartBees() {
+  kill "$PID"
+  watch -n 1 "./build/bees" &
+  PID=$!
+}
+
+
+
+restartBees
+
+while true; do
+  if [ "$(isRebuildNeeded)" == "yes" ]; then
+    echo "Rebuilding"
+    rebuild
+    restartBees
+  fi
+  sleep 1
+done
