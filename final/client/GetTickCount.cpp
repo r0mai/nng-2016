868ec29162d1f12caf95ce66890b0f6051bf8225
Matyas Vegh <matyas.vegh@ericsson.com>
2016-11-25 10:18:52 +0100

    Fix inline GetTickCount

--- final/client/GetTickCount.cpp
+++ final/client/GetTickCount.cpp
@@ -2,7 +2,7 @@
 
 #include "GetTickCount.hpp"
 
-inline int GetTickCount() {
+int GetTickCount() {
 	auto now = std::chrono::steady_clock::now();
 	return now.time_since_epoch().count();
 }
--- final/client/GetTickCount.hpp
+++ final/client/GetTickCount.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 
-inline int GetTickCount();
+int GetTickCount();
